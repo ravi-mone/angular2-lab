7309632cedf5a0085de6922c7c051c98ff12f67c
ravi-tj <ravi.m@techjini.com>
2016-01-08 12:16:45 +0530

    deleted the unwanted files

--- app/helpers/app-injector.js
+++
@@ -1,9 +0,0 @@
-var appInjectorRef;
-exports.appInjector = function (injector) {
-    if (!injector) {
-        return appInjectorRef;
-    }
-    appInjectorRef = injector;
-    return appInjectorRef;
-};
-//# sourceMappingURL=app-injector.js.map
